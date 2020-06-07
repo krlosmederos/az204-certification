f265c4c567ae67dea8fb60e1a2c9b1a32a15a04a
krlosmederos <krlosmederos@gmail.com>
2020-06-07 01:01:22 -0400

    Added labs for Microsoft

---
+++ labs/AZ-204-DevelopingSolutionsforMicrosoftAzure-20200506.02/Allfiles/Labs/04/Starter/AdventureWorks/AdventureWorks.Migrate/Program.cs
@@ -0,0 +1,45 @@
+﻿using AdventureWorks.Context;
+using AdventureWorks.Models;
+using Microsoft.Azure.Cosmos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Program
+{
+    private const string sqlDBConnectionString = "Server=tcp:polysqlsrvrkrlosmederos.database.windows.net,1433;Initial Catalog=AdventureWorks;Persist Security Info=False;User ID=testuser;Password=TestPa55w.rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    private const string cosmosDBConnectionString = "AccountEndpoint=https://polycosmoskrlosmederos.documents.azure.com:443/;AccountKey=BjUHRFQMJnkazAt20QSw50XkWhxEEF8Uolw0m7u95FdWJSI93PWZtYg2gdSVV0rQLbZNZr8sql1ukMXUKqloCA==;";
+
+    public static async Task Main(string[] args)
+    {
+        await Console.Out.WriteLineAsync("Start Migration");
+
+        using AdventureWorksSqlContext context = new AdventureWorksSqlContext(sqlDBConnectionString);
+
+        List<Model> items = await context.Models
+            .Include(m => m.Products)
+            .ToListAsync<Model>();
+
+        await Console.Out.WriteLineAsync($"Total Azure SQL DB Records: {items.Count}");
+
+        using CosmosClient client = new CosmosClient(cosmosDBConnectionString);
+
+        Database database = await client.CreateDatabaseIfNotExistsAsync("Retail");
+
+        Container container = await database.CreateContainerIfNotExistsAsync("Online",
+            partitionKeyPath: $"/{nameof(Model.Category)}",
+            throughput: 1000
+        );
+
+        int count = 0;
+        foreach (var item in items)
+        {
+            ItemResponse<Model> document = await container.UpsertItemAsync<Model>(item);
+            await Console.Out.WriteLineAsync($"Upserted document #{++count:000} [Activity Id: {document.ActivityId}]");
+        }
+
+        await Console.Out.WriteLineAsync($"Total Azure Cosmos DB Documents: {count}");
+    }
+}
---
+++ labs/AZ-204-DevelopingSolutionsforMicrosoftAzure-20200506.02/index.md
@@ -0,0 +1,17 @@
+---
+title: Online Hosted Instructions
+permalink: index.html
+layout: home
+---
+
+# Content Directory
+
+Hyperlinks to each of the lab exercises and demos are listed below.
+
+## Labs
+
+{% assign labs = site.pages | where_exp: "page", "page.url contains '/Instructions/Labs'" %}
+| Module | Lab |
+| --- | --- |
+{% for activity in labs  %}{% if activity.lab.az204Module %}| {{ activity.lab.az204Module }} | [{{ activity.lab.title }}{% if activity.lab.type %} - {{ activity.lab.type }}{% endif %}]({{ site.github.url }}{{ activity.url }}) |
+{% endif %}{% endfor %}
